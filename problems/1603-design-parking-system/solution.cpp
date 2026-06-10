8a4ef9dae64a1e77079f6a27e8beeec1349f328d
Ankit Sharma <ankitsm08@gmail.com>
2026-06-10 21:50:09 +0530

    solve: problem 1603

---
+++ problems/1603-design-parking-system/solution.cpp
@@ -0,0 +1,18 @@
+class ParkingSystem {
+  int slots[3];
+
+public:
+  ParkingSystem(int big, int medium, int small) {
+    slots[0] = big;
+    slots[1] = medium;
+    slots[2] = small;
+  }
+
+  bool addCar(int carType) { return slots[carType - 1]-- > 0; }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
