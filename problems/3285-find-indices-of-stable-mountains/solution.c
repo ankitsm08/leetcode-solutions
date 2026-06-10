c2351a5c7f4094de4876faf366a8922ecc5f20f6
Ankit Sharma <ankitsm08@gmail.com>
2026-06-10 23:31:35 +0530

    solve: problem 3285

---
+++ problems/3285-find-indices-of-stable-mountains/solution.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *stableMountains(int *height, int heightSize, int threshold,
+                     int *returnSize) {
+  int count = 0;
+  for (int i = 1; i < heightSize; i++) {
+    if (height[i - 1] > threshold) {
+      count++;
+    }
+  }
+
+  int *stable = malloc(count * sizeof(int));
+  *returnSize = count;
+  int idx = 0;
+  for (int i = 1; i < heightSize; i++) {
+    if (height[i - 1] > threshold) {
+      stable[idx++] = i;
+    }
+  }
+  return stable;
+}
