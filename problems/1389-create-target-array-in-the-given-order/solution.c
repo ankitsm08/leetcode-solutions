42734e2055d3507ef709901cfe415e77fe89bf11
Ankit Sharma <ankitsm08@gmail.com>
2026-06-11 23:28:31 +0530

    solve: problem 1389

---
+++ problems/1389-create-target-array-in-the-given-order/solution.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *createTargetArray(int *nums, int numsSize, int *index, int indexSize,
+                       int *returnSize) {
+  int *target = malloc(numsSize * sizeof(int));
+  *returnSize = numsSize;
+  int size = 0;
+
+  for (int i = 0; i < numsSize; i++) {
+    for (int j = size; j > index[i]; j--) {
+      target[j] = target[j - 1];
+    }
+
+    target[index[i]] = nums[i];
+    size++;
+  }
+
+  return target;
+}
