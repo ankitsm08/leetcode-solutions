b5e569cd7b88c8e1c50993b7e2a1d45f4921fd11
Ankit Sharma <ankitsm08@gmail.com>
2026-06-15 18:01:32 +0530

    solve: problem 1773

---
+++ problems/1773-count-items-matching-a-rule/solution.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int countMatches(vector<vector<string>> &items, string ruleKey,
+                   string ruleValue) {
+    const int idx = (ruleKey == "color") + 2 * (ruleKey == "name");
+
+    int count = 0;
+    for (const auto &item : items) {
+      if (item[idx] == ruleValue) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+};
