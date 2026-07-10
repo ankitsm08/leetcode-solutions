6c4e3b685691baeef91edd1a43fa3b1307f96244
Ankit Sharma <ankitsm08@gmail.com>
2026-07-11 00:26:31 +0530

    perf: optimize problem 3945

--- problems/3945-digit-frequency-score/solution.c
+++ problems/3945-digit-frequency-score/solution.c
@@ -1,16 +1,6 @@
 int digitFrequencyScore(int n) {
-  int prev = 0, count = 1, score = 0;
-  while (n > 0) {
-    int digit = n % 10;
-    if (digit == prev) {
-      count++;
-    } else {
-      score += prev * count;
-      count = 1;
-    }
-    prev = digit;
-    n /= 10;
-  }
-  score += prev * count;
-  return score;
+  int sum = 0;
+  for (int copy = n; copy > 0; copy /= 10)
+    sum += copy % 10;
+  return sum;
 }
