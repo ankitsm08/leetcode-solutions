abe2c7fd3d61734f008c9e354b1a905778928f5b
Ankit Sharma <ankitsm08@gmail.com>
2026-07-23 19:49:36 +0530

    solve: problem 3894 (easy)

---
+++ problems/3894-traffic-signal-color/solution.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+  string trafficSignal(int timer) {
+    return timer == 0                  ? "Green"
+           : timer == 30               ? "Orange"
+           : timer > 30 && timer <= 90 ? "Red"
+                                       : "Invalid";
+  }
+};
