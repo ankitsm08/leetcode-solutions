023afcc1a736045183916b68d03946d29ed4d520
Ankit Sharma <ankitsm08@gmail.com>
2026-07-26 10:25:09 +0530

    solve: problem 0628 (daily easy)

---
+++ problems/0628-maximum-product-of-three-numbers/solution.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <limits>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int maximumProduct(vector<int> &nums) {
+    int max1, max2, max3;
+    int min1, min2;
+    max1 = max2 = max3 = numeric_limits<int>::min();
+    min1 = min2 = numeric_limits<int>::max();
+
+    for (int num : nums) {
+      if (num > max1) {
+        max3 = max2;
+        max2 = max1;
+        max1 = num;
+      } else if (num > max2) {
+        max3 = max2;
+        max2 = num;
+      } else if (num > max3) {
+        max3 = num;
+      }
+
+      if (num < min1) {
+        min2 = min1;
+        min1 = num;
+      } else if (num < min2) {
+        min2 = num;
+      }
+    }
+
+    return max(max1 * max2 * max3, min1 * min2 * max1);
+  }
+};
