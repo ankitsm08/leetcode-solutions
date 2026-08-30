ed061af52798e4c9779bbb5393220ddfeda99ccd
Ankit Sharma <ankitsm08@gmail.com>
2026-08-31 03:05:10 +0530

    refactor: remove edge case check since general case handles it

--- problems/0160-intersection-of-two-linked-lists/solution.cpp
+++ problems/0160-intersection-of-two-linked-lists/solution.cpp
@@ -10,9 +10,6 @@ struct ListNode {
 class Solution {
 public:
   ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-    if (!headA || !headB)
-      return nullptr;
-
     ListNode *a = headA, *b = headB;
 
     while (a != b) {
